8f0bce0270d8daa00a83e545f8f50d398048f9ca
Your Name <you@example.com>
2025-07-02 12:22:13 +0600

    outlet height fix

--- src/layouts/HomeLayout.jsx
+++ src/layouts/HomeLayout.jsx
@@ -2,17 +2,17 @@ import React from "react";
 import NavBar from "../components/NavBar";
 import { Outlet } from "react-router";
 import Footer from "../components/Footer";
+
 const HomeLayout = () => {
   return (
-    <div>
-      <NavBar></NavBar>
-      <div className=" className='min-h-[calc(100vh-100.08px)]  mb-10">
-        <Outlet></Outlet>
+    <div className="min-h-screen flex flex-col">
+      <NavBar />
+      <div className="flex-grow mb-10">
+        <Outlet />
       </div>
-      <Footer></Footer>
+      <Footer />
     </div>
   );
 };
 
 export default HomeLayout;
-
